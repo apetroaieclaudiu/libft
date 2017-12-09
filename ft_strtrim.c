2cd366389b82391d8b398e6faa28c0daba9d6c56
apetroaieclaudiu <apetroaie.claudiu@gmail.com>
2017-12-09 09:04:41 +0200

    .

---
+++ ft_strtrim.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+char*	ft_strtrim(char const *s)
+{
+	char*	sir;
+	char*	p;
+	char*	start;
+
+	while (*s && (*s != ' ' || *s != '\t' || *s != '\n'))
+	{
+		s++;
+	}
+	start = s;
+	while (*s)
+	{
+		if (*s != ' ' && *s != '\t' && s != '\n')
+			p = s;
+		s++;
+	}
+
+	sir = ft_strnew(ft_strlen(p) - ft_strlen(start));
+
+	ft_strncpy(sir,start, (int) ft_strlen(p) - ft_strlen(start));
+	return sir;
+}
