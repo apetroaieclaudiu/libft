d5c771b21bc86c3faff8286510fe86e32a1ca4cb
Claudiu-alexandru APETROAIE <capetroa@b1p43.academyplus.ro>
2017-12-11 22:07:14 +0200

    Working on

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ FILES		= ft_memset.c \
 				ft_bzero.c \
 				ft_memcpy.c \
 				ft_memccpy.c \
+				ft_memmove.c \
 				ft_strlen.c \
 				ft_memchr.c \
 				ft_memcmp.c \
@@ -49,6 +50,7 @@ FILES		= ft_memset.c \
 				ft_putnbr_fd.c \
 				ft_putstr_fd.c \
 				ft_putendl_fd.c \
+				ft_lstnew.c \
 			
 OBJ			= $(FILES:%.c=%.o)
 
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,6 +1,21 @@
 #include "libft.h"
 
-t_list * ft_lstnew(void const *content, size_t content_size)
+t_list* ft_lstnew(void const *content, size_t content_size)
 {
-	
+	t_list *new;
+
+	new = (t_list*) malloc(sizeof(t_list));
+	if (!new)
+		return NULL;
+	if (!content)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
+	new->content = malloc(sizeof(content_size));
+	if (!new->content)
+		return NULL;
+	ft_memcpy (new->content, content, content_size);
+	new->next = NULL;
+	return new;
 }
